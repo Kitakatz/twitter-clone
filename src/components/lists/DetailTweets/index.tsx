5ca15b03850aa4d15f48fe8ee9b366c454a9c307
Andrew Kita <andrewk0792@yahoo.com>
2022-12-14 09:22:03 -0800

    Fixed AWS SNS native methods from utils folder not returning a response by storing method results in variables and changing interfaces.

--- src/components/lists/DetailTweets/index.tsx
+++ src/components/lists/DetailTweets/index.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { TweetsContext } from '../../../contexts/Tweets';
+import FadeInFromTop from '../../animations/FadeInFromTop';
 import Cards from '../../cards';
 import Layout from '../../layout';
 import { DetailTweetsProps } from './interfaces';
@@ -9,26 +10,26 @@ const DetailTweets: React.FC<DetailTweetsProps> = (props): React.ReactElement | 
   const { state } = useContext(TweetsContext);
 
   const renderItems = (): React.ReactElement[] => {
-    const items = props.items.map((item) => {
+    const items = props.items.map((item, index: number) => {
       return (
-        <div key={ item.id } className='tweet'>
-          <Layout.Row>
-            <Layout.Column>
-              <Cards.Avatar />
-            </Layout.Column>
-            <Layout.Column flex='1'>
-              <div className='handler-section'>
-                <div className='handler'>AWS Amplify</div>
-                <div className='replyTo'>@{item.author}</div>
-                <div className='seperator'>·</div>
-                <div className='timestamp'>Aug 2</div>
-              </div>
-              <div className='paragraph'>
-                {item.tweet}
-              </div>
-            </Layout.Column>
-          </Layout.Row>
-        </div>
+        <FadeInFromTop index={index}>
+          <div key={item.id} className="tweet">
+            <Layout.Row>
+              <Layout.Column>
+                <Cards.Avatar />
+              </Layout.Column>
+              <Layout.Column flex="1">
+                <div className="handler-section">
+                  <div className="handler">AWS Amplify</div>
+                  <div className="replyTo">@{item.author}</div>
+                  <div className="seperator">·</div>
+                  <div className="timestamp">Aug 2</div>
+                </div>
+                <div className="paragraph">{item.tweet}</div>
+              </Layout.Column>
+            </Layout.Row>
+          </div>
+        </FadeInFromTop>
       );
     });
 
